e9b321310394d8a2741e894233642b299947bac3
Andrea Di Cataldi <andrea.dicataldi@gmail.com>
2021-02-11 16:09:33 +0100

    created three blades, products blade complete

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,18 @@
+    <header>
+        <div class="header-logo">
+            <img src="{{ asset('img/marchio-sito.png') }}" alt="">
+        </div>
+        <nav>
+            <ul>
+                <li>
+                    <a href="/">HOME</a>
+                </li>
+                <li>
+                    <a href="prodotti">PRODOTTI</a>
+                </li>
+                <li>
+                    <a href="contatti">CONTATTI</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
---
+++ resources/views/prodotti.blade.php
@@ -0,0 +1,42 @@
+@extends('app')
+
+@section('content')
+    <main>
+        <section class="lunghe">
+            <h2>LE LUNGHE</h2>
+            <div class="paste">
+                @foreach ($paste as $pasta)
+                    @if ($pasta['tipo'] === 'lunga')
+                        <div class="pasta">
+                            <img src="{{ $pasta['src'] }}" alt="">
+                        </div>
+                    @endif
+                @endforeach
+            </div>
+        </section>
+        <section class="corte">
+            <h2>LE CORTE</h2>
+            <div class="paste">
+                @foreach ($paste as $pasta)
+                    @if ($pasta['tipo'] === 'corta')
+                        <div class="pasta">
+                            <img src="{{ $pasta['src'] }}" alt="">
+                        </div>
+                    @endif
+                @endforeach
+            </div>
+        </section>
+        <section class="cortissime">
+            <h2>LE CORTISSIME</h2>
+            <div class="paste">
+                @foreach ($paste as $pasta)
+                    @if ($pasta['tipo'] === 'cortissima')
+                        <div class="pasta">
+                            <img src="{{ $pasta['src'] }}" alt="">
+                        </div>
+                    @endif
+                @endforeach
+            </div>
+        </section>
+    </main>
+@endsection
